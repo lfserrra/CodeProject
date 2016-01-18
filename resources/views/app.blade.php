e75f35bd78dae64c78dbdb62e850fd0a343c6c68
Luis Fernando Serra <lfserrra@gmail.com>
2016-01-18 19:14:55 -0200

    Realizando autenticacao

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,7 +22,7 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body>
+<body ng-app="app">
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
@@ -61,7 +61,7 @@
     </div>
 </nav>
 
-@yield('content')
+<div ng-view></div>
 
 <!-- Scripts -->
 @if(Config::get('app.debug'))
@@ -73,6 +73,13 @@
     <script src="{{ asset('build/js/vendor/angular-messages.min.js') }}"></script>
     <script src="{{ asset('build/js/vendor/ui-bootstrap.min.js') }}"></script>
     <script src="{{ asset('build/js/vendor/navbar.min.js') }}"></script>
+    <script src="{{ asset('build/js/vendor/angular-cookies.min.js') }}"></script>
+    <script src="{{ asset('build/js/vendor/query-string.js') }}"></script>
+    <script src="{{ asset('build/js/vendor/angular-oauth2.min.js') }}"></script>
+
+    <script src="{{ asset('build/js/app.js') }}"></script>
+    <script src="{{ asset('build/js/controllers/login.js') }}"></script>
+    <script src="{{ asset('build/js/controllers/home.js') }}"></script>
 @else
     <script src="{{ elixir('js/all.js') }}"></script>
 @endif
